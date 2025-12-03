1874aa05a0676c8e673f4c9c699645ca64ec3102
kikisan <kiketdule@gmail.com>
2025-12-03 12:55:42 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View , Text} from 'react-native';
+import {s} from './App.style';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import TasksScreen from './src/screens/TasksScreen/TasksScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#ddd" }}>
+          <TasksScreen />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    );
+};
---
+++ src/screens/TasksScreen/TasksScreen.js
@@ -0,0 +1,36 @@
+import { View, Text, FlatList } from "react-native";
+import { s } from "./TasksScreen.style";
+import Header from "../../components/Header/Header";
+import { useState } from "react";
+
+export default function TasksScreen() {
+  const [tasks, setTasks] = useState([
+    { title: "coucou", isCompleted: false },
+    { title: "salut", isCompleted: true },
+    { title: "hello", isCompleted: false },
+    { title: "hi", isCompleted: true },
+    { title: "yo", isCompleted: true },
+  ]);
+
+  return (    
+      <FlatList
+        ListHeaderComponent={<Header />}
+        //   ListFooterComponent={}
+        //   contentContainerStyle={{ padding: 20 }}
+        //   Flatlist est scrollview + évolué et optimisé pour les longues listes
+        //    il necessite une data (tableau de données) et une fonction renderItem (pour rendre chaque élément)
+        //    mais aussi une keyExtractor pour donner une clé unique à chaque élément (ici on utilise l'index)
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <Text
+            style={{
+              textDecorationLine: item.isCompleted ? "line-through" : "none",
+            }}
+          >
+            {item.title}
+          </Text>
+        )}
+      />
+  );
+}
